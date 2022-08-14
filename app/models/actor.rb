9d89aed39294c02f056179f5b795aa2ac3b4a93e
Justin Jeffries <justincyjeffries@gmail.com>
2022-08-14 16:06:46 -0400

    commit tree 1ce718baec84c4418cffa31f7ece89ef7befdeb8

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,24 @@
 class Actor < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :shows, through: :characters
+
+    def full_name
+        "#{self.first_name} #{self.last_name}"
+    end
+
+    def list_roles
+        self.characters.map do |character|
+            "#{character.name} - #{character.show.name}"
+        end
+    end
+
+    def self.alphabetical
+        self.order(:last_name)
+    end
+
+    def self.most_gigs
+        self.all.sort do |a, b|
+            a.characters.count <=> b.characters.count
+        end.last
+    end
 end
